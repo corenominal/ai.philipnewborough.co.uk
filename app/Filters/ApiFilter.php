bb263605e48966fa4a5d1c40fdcfa250d6e1c076
Philip Newborough <corenominal@corenominal.org>
2026-05-08 22:00:40 +0100

    feat: all API calls require admin API key

--- app/Filters/ApiFilter.php
+++ app/Filters/ApiFilter.php
@@ -37,89 +37,112 @@ class ApiFilter implements FilterInterface
     public function before(RequestInterface $request, $arguments = null)
     {
         // CORS Policy
-        header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PATCH, PUT, DELETE');
-        header('Access-Control-Allow-Headers: apikey, user-uuid, email, Content-Type, Content-Length, Accept-Encoding');
-        if ('OPTIONS' === $_SERVER['REQUEST_METHOD']) {
+        header("Access-Control-Allow-Origin: *");
+        header(
+            "Access-Control-Allow-Methods: GET, POST, OPTIONS, PATCH, PUT, DELETE",
+        );
+        header(
+            "Access-Control-Allow-Headers: apikey, user-uuid, email, Content-Type, Content-Length, Accept-Encoding",
+        );
+        if ("OPTIONS" === $_SERVER["REQUEST_METHOD"]) {
             exit();
         }
 
         // Test API key is provided
-        if (!$request->hasHeader('apikey')) {
-            header('HTTP/1.1 401 Unauthorized', true, 401);
-            exit(json_encode(['error' => 'No API key provided.']));
+        if (!$request->hasHeader("apikey")) {
+            header("HTTP/1.1 401 Unauthorized", true, 401);
+            exit(json_encode(["error" => "No API key provided."]));
         }
 
         // Assign the API key
-        $apikey = $request->header('apikey')->getValue();
+        $apikey = $request->header("apikey")->getValue();
 
         // Test API key is not empty
         if (empty($apikey)) {
-            header('HTTP/1.1 401 Unauthorized', true, 401);
-            exit(json_encode(['error' => 'Empty API key provided.']));
+            header("HTTP/1.1 401 Unauthorized", true, 401);
+            exit(json_encode(["error" => "Empty API key provided."]));
         }
 
         // Set success flag
         $success = false;
-        
+
         // Test against 'apikeys.masterKey' .env values first, then database
         // Load API keys configuration
-        $config = config('ApiKeys');
+        $config = config("ApiKeys");
         if ($config->masterKey == $apikey) {
             $success = true;
-            $GLOBALS['is_admin'] = true;
+            $GLOBALS["is_admin"] = true;
         }
 
-        if(!$success){
+        if (!$success) {
             // Reset success flag
             $success = true;
             // Test user UUID is provided
-            if (!$request->hasHeader('user-uuid')) {
-                header('HTTP/1.1 401 Unauthorized', true, 401);
-                exit(json_encode(['error' => 'No user UUID provided.']));
+            if (!$request->hasHeader("user-uuid")) {
+                header("HTTP/1.1 401 Unauthorized", true, 401);
+                exit(json_encode(["error" => "No user UUID provided."]));
             }
             // Get the user UUID from the header
-            $user_uuid = $request->header('user-uuid')->getValue();
+            $user_uuid = $request->header("user-uuid")->getValue();
             // Test user UUID is not empty
             if (empty($user_uuid)) {
-                header('HTTP/1.1 401 Unauthorized', true, 401);
-                exit(json_encode(['error' => 'Empty user UUID provided.']));
+                header("HTTP/1.1 401 Unauthorized", true, 401);
+                exit(json_encode(["error" => "Empty user UUID provided."]));
             }
             // cURL GET request to auth server
             $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, config('Urls')->auth . 'api/keycheck/' . $user_uuid . '/' . $apikey);
+            curl_setopt(
+                $ch,
+                CURLOPT_URL,
+                config("Urls")->auth .
+                    "api/keycheck/" .
+                    $user_uuid .
+                    "/" .
+                    $apikey,
+            );
             // Set 'apikey' header
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array('apikey: ' . config('ApiKeys')->masterKey));
+            curl_setopt($ch, CURLOPT_HTTPHEADER, [
+                "apikey: " . config("ApiKeys")->masterKey,
+            ]);
             // Return the transfer as a string
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
             $response = curl_exec($ch);
             $response = json_decode($response);
 
             // Test for error response
-            if(isset($response->error)){
+            if (isset($response->error)) {
                 // Error response, set flag to false
                 $success = false;
             }
 
             // Check if the response contains is_admin bool value
-            if(isset($response->is_admin)){
+            if (isset($response->is_admin)) {
                 // If the user is an admin we need to set a var that will be used in the controllers to allow access to admin routes
-                if($response->is_admin === true){
+                if ($response->is_admin === true) {
                     // Set a global variable that can be accessed in the controllers to allow access to admin routes
-                    $GLOBALS['is_admin'] = true;
+                    $GLOBALS["is_admin"] = true;
                 }
             }
         }
 
         // Test flag
         if (!$success) {
-            header('HTTP/1.1 401 Unauthorized', true, 401);
-            exit(json_encode(['error' => 'Invalid API key.']));
+            header("HTTP/1.1 401 Unauthorized", true, 401);
+            exit(json_encode(["error" => "Invalid API key."]));
+        }
+
+        // Check if the user is an admin
+        if (empty($GLOBALS["is_admin"])) {
+            header("HTTP/1.1 401 Unauthorized", true, 401);
+            exit(json_encode(["error" => "Admin privileges required."]));
         }
     }
 
-    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
-    {
+    public function after(
+        RequestInterface $request,
+        ResponseInterface $response,
+        $arguments = null,
+    ) {
         // Nothing to do here
     }
 }
